f391efa0ce6bb4799551436d4304f1aa0789719c
ArmoredPangolin <victor.lawrence.noon@gmail.com>
2020-08-05 16:48:21 +0200

    Parser: OK Instantiation du State Initiale: Encours

--- src/main/kotlin/n/puzzle/Main.kt
+++ src/main/kotlin/n/puzzle/Main.kt
@@ -4,25 +4,25 @@ import java.io.File
 
 
 @ExperimentalStdlibApi
-fun File.checkFileFormat(): String {
-    var sizeNPuzzle = 0
+fun File.checkAndExtract(): Pair<String, UIntArray?> {
 
     val lines = this.readLines().filter { it.startsWith("#") }.map { it.split("#")[0] }.toMutableList()
-    val size = lines.removeFirst().toUIntOrNull() ?: return "Invalid File: Error Invalid N-Puzzle size: Not An UInt"
-    val array = lines.flatMap {
-        it.split(" ").map {
-            it.toUIntOrNull() ?: return "Invalid File: Error Invalid Element $it in N-Puzzle"
-        }
+    val size = lines.removeFirst().toUIntOrNull() ?: return "Invalid File: Error Invalid N-Puzzle size: Not An UInt" to null
+
+    val finalArray = lines.flatMap {
+        it.split(" ").map { it.toUIntOrNull() ?: return "Invalid File: Error Invalid Element ${it} in N-Puzzle" to null }
     }.toUIntArray()
 
+    if(finalArray.toSet().size.toUInt() != (size * size))
+        return "Invalid File: Some Elements are duplicated" to null
+
+    val nbElem = (size * size)
+    if (finalArray.any { (it >= (nbElem))  || it < 0U })
+        return "Invalid File: Some Elements are out of Range" to null
 
 
-    return "Valid"
-}
 
-fun File.extractBaseState(): UIntArray {
-
-    return (UIntArray(0))
+    return Pair("Valid", finalArray)
 }
 
 
@@ -31,10 +31,10 @@ fun main(args: Array<String>) {
 
     for (arg in args) {
         val file = File(arg)
-        if (file.checkFileFormat() == "Valid") {
-            file.extractBaseState() // then send to solver
-        }
+        val extractedFile = file.checkAndExtract()
+        if (extractedFile.first == "Valid") {
 
+        }
     }
 
 }
