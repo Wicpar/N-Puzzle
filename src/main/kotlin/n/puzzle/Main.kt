b344852f58f266fd88d881a8a25387c100bd4b02
ArmoredPangolin <victor.lawrence.noon@gmail.com>
2020-08-05 17:09:05 +0200

    Parser: OK Instantiation du State Initiale: En debug

--- src/main/kotlin/n/puzzle/Main.kt
+++ src/main/kotlin/n/puzzle/Main.kt
@@ -1,31 +1,61 @@
 package n.puzzle
 
+import n.puzzle.state.Coord
+import n.puzzle.state.State
 import java.io.File
 
 
+@ExperimentalUnsignedTypes
 @ExperimentalStdlibApi
-fun File.checkAndExtract(): Pair<String, UIntArray?> {
-
+fun File.checkAndExtract(): Pair<String, State?> {
     val lines = this.readLines().filter { it.startsWith("#") }.map { it.split("#")[0] }.toMutableList()
-    val size = lines.removeFirst().toUIntOrNull() ?: return "Invalid File: Error Invalid N-Puzzle size: Not An UInt" to null
+    val size =
+        lines.removeFirst().toUIntOrNull() ?: return "Invalid File: Error Invalid N-Puzzle size: Not An UInt" to null
 
     val finalArray = lines.flatMap {
-        it.split(" ").map { it.toUIntOrNull() ?: return "Invalid File: Error Invalid Element ${it} in N-Puzzle" to null }
+        it.split(" ")
+            .map { it.toUIntOrNull() ?: return "Invalid File: Error Invalid Element ${it} in N-Puzzle" to null }
     }.toUIntArray()
 
-    if(finalArray.toSet().size.toUInt() != (size * size))
+    if (finalArray.toSet().size.toUInt() != (size * size))
         return "Invalid File: Some Elements are duplicated" to null
 
     val nbElem = (size * size)
-    if (finalArray.any { (it >= (nbElem))  || it < 0U })
+    if (finalArray.any { (it >= (nbElem)) || it < 0U })
         return "Invalid File: Some Elements are out of Range" to null
 
+    val initialState =
+        State(finalArray, size, Coord(finalArray.indexOf(0U).toUInt() % size, finalArray.indexOf(0U).toUInt() / size))
 
-
-    return Pair("Valid", finalArray)
+    return Pair("Valid", initialState)
 }
 
+@ExperimentalUnsignedTypes
+@ExperimentalStdlibApi
+fun List<String>.checkAndExtract(): Pair<String, State?> {
+    val lines = this.filter { it.startsWith("#") }.map { it.split("#")[0] }.toMutableList()
+    val size =
+        lines.removeFirst().toUIntOrNull() ?: return "Invalid File: Error Invalid N-Puzzle size: Not An UInt" to null
 
+    val finalArray = lines.flatMap {
+        it.split(" ")
+            .map { it.toUIntOrNull() ?: return "Invalid File: Error Invalid Element ${it} in N-Puzzle" to null }
+    }.toUIntArray()
+
+    if (finalArray.toSet().size.toUInt() != (size * size))
+        return "Invalid File: Some Elements are duplicated" to null
+
+    val nbElem = (size * size)
+    if (finalArray.any { (it >= (nbElem)) || it < 0U })
+        return "Invalid File: Some Elements are out of Range" to null
+
+    val initialState =
+        State(finalArray, size, Coord(finalArray.indexOf(0U).toUInt() % size, finalArray.indexOf(0U).toUInt() / size))
+
+    return Pair("Valid", initialState)
+}
+
+@ExperimentalUnsignedTypes
 @ExperimentalStdlibApi
 fun main(args: Array<String>) {
 
