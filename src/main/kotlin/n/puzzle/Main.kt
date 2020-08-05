9ad2e8a160a9dc5d84fd6ca11403000d7d7d5c62
ArmoredPangolin <victor.lawrence.noon@gmail.com>
2020-08-05 16:09:01 +0200

    Entry passer in dev

--- src/main/kotlin/n/puzzle/Main.kt
+++ src/main/kotlin/n/puzzle/Main.kt
@@ -1,10 +1,41 @@
 package n.puzzle
+
 import java.io.File
 
+
+@ExperimentalStdlibApi
+fun File.checkFileFormat(): String {
+    var sizeNPuzzle = 0
+
+    val lines = this.readLines().filter { it.startsWith("#") }.map { it.split("#")[0] }.toMutableList()
+    val size = lines.removeFirst().toUIntOrNull() ?: return "Invalid File: Error Invalid N-Puzzle size: Not An UInt"
+
+
+    for (line in lines) {
+        if (line.split(" ").any { it.toUIntOrNull() == null })
+            return "Invalid File: Error Invalid Element in N-Puzzle"
+    }
+
+
+
+    return "Valid"
+}
+
+fun File.extractBaseState(): UIntArray {
+
+    return (UIntArray(0))
+}
+
+
+@ExperimentalStdlibApi
 fun main(args: Array<String>) {
 
     for (arg in args) {
-        var file : File = File(arg)
+        val file = File(arg)
+        if (file.checkFileFormat() == "Valid") {
+            file.extractBaseState() // then send to solver
+        }
+
     }
 
 }
